295f319b6650d4de73fb4a0d25d67c11239c1118
Sandip Gahlot <sandip.gahlot@capco.com>
2020-07-20 19:09:24 -0400

    updated menuItem withRouter + added BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+
 import './App.css';
 import HomePage from "./pages/homepage/homepae.component";
 import './pages/homepage/homepage.styles.scss';
 
 function App() {
   return (
-    <div >
-      <HomePage/>
+    <div>
+      {/*<BrowserRouter>*/}
+      {/*  <Switch>*/}
+      {/*    <Route exact path={'/'} component={HomePage} />*/}
+      {/*  </Switch>*/}
+      {/*</BrowserRouter>*/}
+      <BrowserRouter>
+        <HomePage/>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -49,8 +49,8 @@ class Directory extends Component {
     return (
       <div className={'directory-menu'}>
         {
-          this.state.sections.map(({title, imageUrl, id, linkUrl, size}) => (
-            <MenuItem key={id} title={title} imageUrl={imageUrl} linkUrl={linkUrl} size={size} />
+          this.state.sections.map(({id, ...otherSectionProps}) => (
+            <MenuItem key={id} {...otherSectionProps} />
           ))
         }
       </div>
