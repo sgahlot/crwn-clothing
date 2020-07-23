ee7e7e7a6c14165b286e6e67a78cd9b815e3f188
Sandip Gahlot <sandip.gahlot@capco.com>
2020-07-22 23:04:46 -0400

    added header + links

--- src/App.js
+++ src/App.js
@@ -5,20 +5,19 @@ import './App.css';
 import HomePage from "./pages/homepage/homepae.component";
 import ShopPage from "./pages/shop/shop.component";
 import './pages/homepage/homepage.styles.scss';
+import Header from "./components/header/header.component";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
+        <Header/>
         <Switch>
           <Route exact path={'/'} component={HomePage} />
           <Route exact path={'/shop'} component={ShopPage} />
         </Switch>
       </BrowserRouter>
 
-      {/*<BrowserRouter>*/}
-      {/*  <HomePage/>*/}
-      {/*</BrowserRouter>*/}
     </div>
   );
 }
