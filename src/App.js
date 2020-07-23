6af0e6cd124120b2f0099c529a9b935d7c1ae391
Sandip Gahlot <sandip.gahlot@capco.com>
2020-07-22 22:50:33 -0400

    updated with shop page + collection preview + collection item

--- src/App.js
+++ src/App.js
@@ -3,19 +3,22 @@ import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
 import './App.css';
 import HomePage from "./pages/homepage/homepae.component";
+import ShopPage from "./pages/shop/shop.component";
 import './pages/homepage/homepage.styles.scss';
 
 function App() {
   return (
     <div>
-      {/*<BrowserRouter>*/}
-      {/*  <Switch>*/}
-      {/*    <Route exact path={'/'} component={HomePage} />*/}
-      {/*  </Switch>*/}
-      {/*</BrowserRouter>*/}
       <BrowserRouter>
-        <HomePage/>
+        <Switch>
+          <Route exact path={'/'} component={HomePage} />
+          <Route exact path={'/shop'} component={ShopPage} />
+        </Switch>
       </BrowserRouter>
+
+      {/*<BrowserRouter>*/}
+      {/*  <HomePage/>*/}
+      {/*</BrowserRouter>*/}
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -2,46 +2,14 @@ import React, {Component} from "react";
 import MenuItem from "../menu-item/menu-item.component";
 
 import './directory-styles.scss';
+import SECTIONS_DATA from "../../sections.data";
 
 class Directory extends Component {
   constructor() {
     super();
 
     this.state = {
-      sections: [
-        {
-          id: 1,
-          title: 'hats',
-          imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-          linkUrl: 'shop/hats'
-        },
-        {
-          id: 2,
-          title: 'jackets',
-          imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-          linkUrl: 'shop/jackets'
-        },
-        {
-          id: 3,
-          title: 'sneakers',
-          imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-          linkUrl: 'shop/sneakers'
-        },
-        {
-          id: 4,
-          title: 'womens',
-          imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-          linkUrl: 'shop/womens',
-          size: 'large'
-        },
-        {
-          id: 5,
-          title: 'mens',
-          imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-          linkUrl: 'shop/mens',
-          size: 'large'
-        }
-      ]
+      sections: SECTIONS_DATA
     }
   }
 
