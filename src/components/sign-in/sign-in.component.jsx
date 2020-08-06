911827e8dcc68c211b6ab6ab7daa3ef860525755
Sandip Gahlot <sandip.gahlot@capco.com>
2020-08-05 23:37:38 -0400

    added sign-in with email/password submit

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import React, {Component} from "react";
 import './sign-in.styles.scss';
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
-import {signInWithGoogle} from "../../firebase/firebase.util";
+import {auth, signInWithGoogle} from "../../firebase/firebase.util";
 
 class SignIn extends Component {
   constructor(props) {
@@ -15,10 +15,17 @@ class SignIn extends Component {
     }
   }
 
-  handleSubmit = event => {
+  handleSubmit = async event => {
     event.preventDefault();
 
-    this.setState({email: '', password: ''})
+    const {email, password} = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password)
+      this.setState({email: '', password: ''})
+    } catch(error) {
+      console.error();
+    }
   }
 
   handleChange = event => {
